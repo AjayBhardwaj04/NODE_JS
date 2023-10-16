d13de57d42d0907c79b4123d0131a9ea550b4d10
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-16 15:37:45 +0530

    node_js code

---
+++ index.js
@@ -0,0 +1,19 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("<h1>Jai Shree Ram !!!</h1 ");
+  } else if (req.url === "/about") {
+    res.end("<h1>About page</h1>");
+  } else if (req.url === "/contact") {
+    res.end("<h1>contact page</h1>");
+  } else if(req.url === "/project"){
+    res.end("<h1>Project page</h1>")
+  } else{
+    res.end("<h1>Page NOT Found!!!!</h1>")
+  }
+});
+
+server.listen(4000, () => {
+  console.log("Server is Runing ~");
+});
