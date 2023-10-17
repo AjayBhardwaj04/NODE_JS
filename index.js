c37b81a0b4255e277a43a9826f8459ac24e47c73
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-17 06:53:34 +0530

    modified: code

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import http from "http";
+import funasdfsa from "./funtion.js";
+
+console.log(funasdfsa());
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("<h1>Jai Shree Ram !!!</h1 ");
+  if (req.url === "/") { 
+    res.end(`<h1>Jai Shree Ram : ${funasdfsa()}</h1` );
   } else if (req.url === "/about") {
     res.end("<h1>About page</h1>");
   } else if (req.url === "/contact") {
     res.end("<h1>contact page</h1>");
-  } else if(req.url === "/project"){
-    res.end("<h1>Project page</h1>")
-  } else{
-    res.end("<h1>Page NOT Found!!!!</h1>")
+  } else if (req.url === "/project") {
+    res.end("<h1>Project page</h1>");
+  } else {
+    res.end("<h1>Page NOT Found!!!!</h1>");
   }
 });
 
