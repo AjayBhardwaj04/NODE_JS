a5865cb7c15192cbac9309e1a14963e75baad473
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2023-10-23 10:30:32 +0530

    node,js codes

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import express from "express";
-// import path from "path";
+import path from "path";
 
 const app = express();
 
+const users = [];
 
-app.set("view engine" ,"ejs" );
+// Usind all Meddlewares
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
- res.render("cdd" ,{name:"Ajay Raj"});
+app.get("/add", (req, res) => {
+  // res.render("index");
+  res.send("Good !!")
 });
 
+app.get("/success", (req, res) => {
+  res.render("success"); 
+});
+
+app.post("/contact", (req, res) => {
+  users.push({ username: req.body.name, email: req.body.email });
+  res.redirect("/success");
+});
+
+app.get("/user", (req,res) => {
+  res.json({
+    users,
+  })
+} )
+
 app.listen(4000, () => {
-  console.log("App Srever Runig PORT : 4000");
+  console.log("server is conected");
 });
